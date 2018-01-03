56e41c1839788b7f6753da3cdf9b34f7ecab3682
Andrey Larin <lestex@gmail.com>
2018-01-03 21:45:19 +0300

    updates tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,7 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+    f = host.file('/etc/default/docker')
 
     assert f.exists
     assert f.user == 'root'
