376819e3e6d381f56e89f30bb55890b7e1440020
Andrey Larin <lestex@gmail.com>
2018-01-03 23:00:49 +0300

    adds tests for package and service

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,9 +11,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '/etc/docker/',
     '/lib/systemd/system/docker.service'
 ])
-def test_hosts_file(host, file):
+def test_docker_file(host, file):
     f = host.file(file)
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+@pytest.mark.parametrize('name', [
+    'docker-ce'
+])
+def test_package(host, name):
+    pkg = host.package(name)
+    assert pkg.is_installed
+
+
+@pytest.mark.parametrize('name', [
+    'docker'
+])
+def test_service(host, name):
+    service = host.service(name)
+    assert service.is_enabled
+    assert service.is_running
